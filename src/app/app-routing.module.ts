1f1f2878a32f01f21c3b13b0fbaab482a6e9e95d
Carlos <j.macedo645@gmail.com>
2020-11-05 23:34:30 -0300

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
 
 // welcome 
 const routes: Routes = [
-    { path:'', component: LoginComponent},
-    { path:'login', component: LoginComponent},
-    { path:'welcome/:name', component: WelcomeComponent},
-    { path:'**', component: ErrorComponent}
-
+    { path:'', component: LoginComponent },
+    { path:'login', component: LoginComponent },
+    { path:'welcome/:name', component: WelcomeComponent },
+    { path:'todos', component: ListTodosComponent },
+    { path:'**', component: ErrorComponent }
 ];
 
 @NgModule({
