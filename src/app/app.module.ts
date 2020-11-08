e7b6c1256063da4890a2e87eb67e06a42b6da1fa
Carlos <j.macedo645@gmail.com>
2020-11-08 10:16:02 -0300

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,9 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './menu/menu.component';  
 import { FooterComponent } from './footer/footer.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { FooterComponent } from './footer/footer.component';
     ErrorComponent,
     ListTodosComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -15,14 +16,16 @@ export class LoginComponent implements OnInit {
   //Router
   //Angular.giveMeRouter
   //Dependency Injection
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private hardcodeAuthenticationServices: HardcodedAuthenticationService) { }
 
   ngOnInit() {
   }
 
   handleLogin() {
     //console.log(this.username);
-    if(this.username==="in28minutes" && this.password === 'dummy') {
+    //if(this.username==="in28minutes" && this.password === 'dummy') {
+      if(this.hardcodeAuthenticationServices.authenticate(this.username, this.password)) {
       //Redirect to Welcome Page
       this.router.navigate(['welcome', this.username])
       this.invalidLogin = false
